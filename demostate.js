e2abb9b3bd5b81ae098d001ad30d2f18c56be24f
phadatul <phadatul@gmail.com>
2023-12-16 12:25:58 +0530

    New JS File
    Adding state related code

---
+++ demostate.js
@@ -0,0 +1,39 @@
+
+class Clock extends React.Component{
+
+    constructor(props){
+       super(props)
+       this.state={mydate:new Date(),
+                    isToggle:true}
+    }
+    componentDidMount(){
+        this.myId=setInterval(()=>this.changeDate(),1000)
+    }
+
+    componentWillUnmount(){
+               clearInterval(this.myId)
+    }
+
+    changeDate(){
+        this.setState({mydate:new Date()})
+    }
+    btnClick=()=>{
+        this.setState(state=>({isToggle:!state.isToggle}))
+    }
+
+    render(){
+
+        return(
+            <div className="container">
+                <h2>Hello , state..</h2>
+                <h2>Time is : {this.state.mydate.toLocaleTimeString()}</h2>
+                <button className="btn btn-success" onClick={this.btnClick}>
+                    {this.state.isToggle?'ON':'OFF'}</button>
+            </div>
+        );
+    }
+
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(<Clock></Clock>);
